a402c3c26edd3023916f006ca46d398775960bc0
Rahul Desai <rahuldesaislice@Rahuls-MBP-2.modem>
2025-03-26 10:37:58 +1100

    <meta name=robots content=noindex>

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
+        <meta name="robots" content="noindex" />
       </head>
       <body className={`${inter.className} min-h-screen w-full overflow-x-hidden`}>
         <GTMProvider>
